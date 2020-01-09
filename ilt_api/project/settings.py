efaceccab928d7aad77c90e9b3f9cadc18c6587e
Clément Roig <clm.roig@gmail.com>
2020-01-09 11:25:50 +0100

    Trying to debug 500 prod error

--- ilt_api/project/settings.py
+++ ilt_api/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("ENV") == "PRODUCTION":
-    DEBUG = True
+    DEBUG = False
 else:
     DEBUG = True
 
